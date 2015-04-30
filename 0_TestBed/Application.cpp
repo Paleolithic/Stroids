1c87a2c54c6a24ae052c90f18497bb844f2971ef
unknown <jxc4954@GDD-39.main.ad.rit.edu>
2015-04-29 21:37:31 -0400

    Making our code work well together.

--- 0_TestBed/Application.cpp
+++ 0_TestBed/Application.cpp
@@ -10,26 +10,24 @@ void ApplicationClass::InitUserAppVariables()
 	//= m_pMeshMngr->GetInstanceByName("Steve");
 	m_pMeshMngr->LoadModelUnthreaded("Minecraft\\MC_Pig.obj", "Pig");
 
-
+	srand (time(NULL));
 	for (int nCreepers = 0; nCreepers  < 2; nCreepers ++)
 	{
-		//m_pMeshMngr->LoadModelUnthreaded("Minecraft\\MC_Creeper.obj", "Creeper");
-	//}
-		m_pMeshMngr->LoadModelUnthreaded("Minecraft\\MC_Creeper.obj", "Creeper");
-		m_sSelectedObject = "Creeper";
+		float yPos = rand() % 3 + -3;
+		m_pMeshMngr->LoadModelUnthreaded("Minecraft\\MC_Creeper.obj", "Creeper", glm::translate(vector3(-3.0f, yPos, 0.0f)));
+		m_sSelectedObject = "Steve";
 	}
 
 }
 void ApplicationClass::Update (void)
 {
-	
+	// Time stuff 
+	// TODO: REWRITE
 	m_pSystem->UpdateTime();//Update the system
 	m_pMeshMngr->Update(); //Update the mesh information
 	float fTimeSpan = m_pSystem->LapClock();
 	static float fRunTime = 0.0f;
 	fRunTime += fTimeSpan;
-
-	// Time counter
 	static float fTotalTime = 0.0f;
 	float fLapDifference = m_pSystem->StopClock();
 	fTotalTime += fLapDifference;
@@ -49,8 +47,9 @@ void ApplicationClass::Update (void)
 	matrix4 m_m4Pig = sRotate * sTranslate * sOrbit * glm::translate(pigPos);
 	
 	// Set Pig Model Matrix
+	m_pMeshMngr->SetModelMatrix(m_m4SelectedObject, m_sSelectedObject);
 	m_pMeshMngr->SetModelMatrix(m_m4Pig, "Pig");
-
+	
 	//First person camera movement
 	if(m_bFPC == true)
 		CameraRotation();
@@ -58,9 +57,9 @@ void ApplicationClass::Update (void)
 	if(m_bArcBall == true)
 	{
 		ArcBall();
-		m_pMeshMngr->SetModelMatrix(m_m4ObjectTranslation * m_m4ObjectOrientation, m_sSelectedObject); //Setting up the Model Matrix
 	}
 
+	// CATHY SHIT
 	float ScreenLength = 3.0f;
 	if(fRunTime > ScreenLength)
 	{
@@ -68,55 +67,36 @@ void ApplicationClass::Update (void)
 	}
 	float ScreenPercent = MapValue(fRunTime, 0.0f, ScreenLength, 0.0f, 1.0f);
 
-	float windowH = MapValue((float)m_pSystem->GetWindowHeight(), 0.0f, 720.0f, 0.0f, (22.5f/1.8f));
-	float windowW = MapValue((float)m_pSystem->GetWindowWidth(), 0.0f, 1280.0f, 0.0f, 22.5f);
-	float CreeperY = windowH/2;
-	float CreeperX = windowW/2;
-	int randX = rand() % (int)windowW;
-	int arrayVal[2];
-	for(int i = 0; i < 2; i++)
-	{
-		arrayVal[i] = rand() % (int)windowW;
-	}
-
-	for(int creepernum = 0; creepernum < 2; creepernum++)
-	{
-		vector3 Start(arrayVal[creepernum], 0.0f,  0.0f);
-		vector3 Finish(-arrayVal[creepernum], 0.0f, 0.0f );
-	
-	
-	
-	#ifdef USINGLERP
-	vector3 v3Lerp;
-	v3Lerp.x = MapValue(ScreenPercent, 0.0f, 1.0f, Start.x, Finish.x);
-	v3Lerp.y = MapValue(ScreenPercent, 0.0f, 1.0f, Start.y, Finish.y);
-	v3Lerp.z = MapValue(ScreenPercent, 0.0f, 1.0f, Start.z, Finish.z);
-	m_m4ObjectOrientation = glm::translate(v3Lerp);
-	#endif  USINGLERP 
-	
-
-	
-	//v3Lerp = glm::lerp(Start,Finish,ScreenPercent);
-	/*m_m4ObjectOrientation =	glm::rotate(MEIDENTITY, v3Lerp.x, MEAXISX) *
-								glm::rotate(MEIDENTITY, v3Lerp.y, MEAXISY) *
-								glm::rotate(MEIDENTITY, v3Lerp.z, MEAXISZ);*
-								glm::translate(v3Lerp);
-	#endif  USINGLERP */
-
-	m_pMeshMngr->SetModelMatrix(m_m4ObjectTranslation * m_m4ObjectOrientation, m_sSelectedObject);
-	m_pMeshMngr->AddAxisToQueue(m_m4ObjectTranslation * m_m4ObjectOrientation * glm::scale(vector3(3.0f)));
-
-	m_pMeshMngr->AddInstanceToRenderList("ALL");
-	}
-
-	//m_pMeshMngr->AddAxisToQueue(glm::translate(vector3(0,0,0)));
 	
 	float width = MapValue((float)m_pSystem->GetWindowWidth(), 0.0f, 1280.0f, 0.0f, 22.5f);
 	float height = MapValue((float)m_pSystem->GetWindowHeight(), 0.0f, 720.0f, 0.0f, (22.5f/1.8f));
-	//float height = m_pSystem->GetWindowHeight;
 	
 	float half_height = height/2;
 	float half_width  = width/2;
+
+	for(int creepernum = 0; creepernum < 2; creepernum++)
+	{
+		vector3 tempBOCentroid = vector3(0.0f, 0.0f, 0.0f);
+		String tempName = m_pMeshMngr->GetNameOfInstanceByIndex(creepernum+2);
+		BoundingObjectClass* tempBO = m_pMeshMngr->GetBoundingObject(tempName);
+		tempBOCentroid.y = tempBO->GetCentroidGlobal().y - tempBO->GetCentroidLocal().y;
+		std::cout << tempBOCentroid.y << std::endl;
+
+		#ifdef USINGLERP
+		vector3 v3Lerp;
+		v3Lerp.x = MapValue(ScreenPercent, 0.0f, 1.0f, -half_width, half_width);
+		v3Lerp.y = tempBOCentroid.y;
+		v3Lerp.z = 0.0f;
+		//m_m4ObjectOrientation = glm::translate(v3Lerp);
+		#endif  USINGLERP 
+	
+		m_pMeshMngr->SetModelMatrix(glm::translate(v3Lerp), tempName);
+		//m_pMeshMngr->AddAxisToQueue(m_m4ObjectTranslation * m_m4ObjectOrientation * glm::scale(vector3(3.0f)));
+
+		m_pMeshMngr->AddInstanceToRenderList("ALL");
+	}
+	
+	// Jared's shit
 
 	//Top-Right corner
 	m_pMeshMngr->AddCubeToQueue(glm::translate(vector3(width/4, height/4,0))* glm::scale(vector3(width/2, height/2, 0)), MERED, WIRE);
